0b80f751322e4780b2298f15d8eafcde0c7ca498
VISHNU M K <mail.vishnumk@gmail.com>
2023-10-21 12:53:43 +0530

    fix: uploading to cloudinary

--- Middleware/Cloudinary.js
+++ Middleware/Cloudinary.js
@@ -2,12 +2,6 @@ import fs from 'fs'
 import cloudinary from "cloudinary"
 import dotenv from "dotenv"
 import sha256 from "sha256"
-import path from "path"
-import {fileURLToPath} from "url"
-
-const moduleURL = new URL(import.meta.url);
-const filePath = fileURLToPath(moduleURL);
-const directoryPath = path.dirname(filePath);
 
 dotenv.config()
 
@@ -19,13 +13,12 @@ cloudinary.config({
 
 export const uploadCloud = async (image,num) => {
     const uuid = sha256(image)
-    const path = num===1 ? path.join(directoryPath,"/../Temp/ProfilePics/"+image+"") : num===2 ? path.join(directoryPath,"/../Temp/Audios/"+image+"") : path.join(directoryPath,"/../Temp/Pdfs/"+image+"")
+    const path = num===1 ? "./Temp/ProfilePics/"+image+"" : num===2 ? "./Temp/Audios/"+image+"" : "./Temp/Pdfs/"+image+""
     const obj = num===1 ? {public_id:uuid} : num===2 ? {resource_type: "raw",public_id:uuid} : {resource_type: "raw",public_id:uuid}
     await cloudinary.v2.uploader.upload(path,obj,(err,result)=>{
-        console.log(err);
-        num === 1 ? fs.unlink(path.join(directoryPath,"/../Temp/ProfilePics/"+image+""),(err, response)=>{console.log(err);}) :
-        num === 2 ? fs.unlink(path.join(directoryPath,"/../Temp/Audios/"+image+""),(err, response)=>{}) : 
-        fs.unlink(path.join(directoryPath,"/../Temp/Pdfs/"+image+""),(err, response)=>{})
+        num === 1 ? fs.unlink("./Temp/ProfilePics/"+image+"",(err, response)=>{}) :
+        num === 2 ? fs.unlink("./Temp/Audios/"+image+"",(err, response)=>{}) : 
+        fs.unlink("./Temp/Pdfs/"+image+"",(err, response)=>{})
     })
     return uuid
 }
